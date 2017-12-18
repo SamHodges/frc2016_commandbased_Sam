3781cf634932921abbfd711055598b945d5f9261
Sam <shodges2019@francisparker.org>
2017-12-18 12:46:23 -0800

    robot programming

--- src/org/usfirst/frc/team2485/robot/Robot.java
+++ src/org/usfirst/frc/team2485/robot/Robot.java
@@ -8,8 +8,7 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import org.usfirst.frc.team2485.robot.commands.ExampleCommand;
-import org.usfirst.frc.team2485.robot.subsystems.ExampleSubsystem;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -20,11 +19,7 @@ import org.usfirst.frc.team2485.robot.subsystems.ExampleSubsystem;
  */
 public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
-	public static OI oi;
-
-	Command autonomousCommand;
-	SendableChooser<Command> chooser = new SendableChooser<>();
+	
 
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -32,10 +27,8 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void robotInit() {
-		oi = new OI();
-		chooser.addDefault("Default Auto", new ExampleCommand());
-		// chooser.addObject("My Auto", new MyAutoCommand());
-		SmartDashboard.putData("Auto mode", chooser);
+		OI.init();
+		RobotMap.init();
 	}
 
 	/**
@@ -66,7 +59,6 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousInit() {
-		autonomousCommand = chooser.getSelected();
 
 		/*
 		 * String autoSelected = SmartDashboard.getString("Auto Selector",
@@ -75,9 +67,6 @@ public class Robot extends IterativeRobot {
 		 * autonomousCommand = new ExampleCommand(); break; }
 		 */
 
-		// schedule the autonomous command (example)
-		if (autonomousCommand != null)
-			autonomousCommand.start();
 	}
 
 	/**
@@ -94,8 +83,7 @@ public class Robot extends IterativeRobot {
 		// teleop starts running. If you want the autonomous to
 		// continue until interrupted by another command, remove
 		// this line or comment it out.
-		if (autonomousCommand != null)
-			autonomousCommand.cancel();
+
 	}
 
 	/**
--- src/org/usfirst/frc/team2485/robot/RobotMap.java
+++ src/org/usfirst/frc/team2485/robot/RobotMap.java
@@ -1,5 +1,16 @@
 package org.usfirst.frc.team2485.robot;
 
+import org.usfirst.frc.team2485.robot.subsystems.BoulderStager;
+import org.usfirst.frc.team2485.robot.subsystems.DriveTrain;
+import org.usfirst.frc.team2485.robot.subsystems.Shooter;
+import org.usfirst.frc.team2485.util.SpeedControllerWrapper;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.VictorSP;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -7,6 +18,72 @@ package org.usfirst.frc.team2485.robot;
  * floating around.
  */
 public class RobotMap {
+	public static AHRS ahrs;
+	public static double ROBOT_WIDTH = 27;
+	
+	//set motors
+	public static VictorSP lDriveMotor1;
+	public static VictorSP lDriveMotor2;
+	public static VictorSP lDriveMotor3;
+	public static VictorSP rDriveMotor1;
+	public static VictorSP rDriveMotor2;
+	public static VictorSP rDriveMotor3;
+	public static VictorSP speedControllerShooter;
+	public static Encoder encoderShooter;
+	public static Solenoid solenoid1;
+	public static Solenoid solenoid2;
+	
+	
+	
+	public static final int speedControllerPortShooter = 0;
+	public static final int encoderShooterPortA = 6;
+	public static final int encoderShooterPortB = 7;
+	
+	
+	
+	public static final int lDriveMotorPort1 = 2;
+	public static final int lDriveMotorPort2 = 3;
+	public static final int lDriveMotorPort3 = 4;
+	public static final int rDriveMotorPort1 = 5;
+	public static final int rDriveMotorPort2 = 6;
+	public static final int rDriveMotorPort3 = 7;
+	
+	public static final int solenoid1Port = 6;
+	public static final int solenoid2Port = 7;
+	
+	public static SpeedControllerWrapper leftDriveMotor;
+	public static SpeedControllerWrapper rightDriveMotor;
+	
+	public static DriveTrain driveTrain;
+	public static Shooter shooter;
+	public static BoulderStager boulderStager;
+	
+	public static void init(){
+		lDriveMotor1 = new VictorSP(lDriveMotorPort1);
+		lDriveMotor2 = new VictorSP(lDriveMotorPort2);
+		lDriveMotor3 = new VictorSP(lDriveMotorPort3);
+		rDriveMotor1 = new VictorSP(rDriveMotorPort1);
+		rDriveMotor2 = new VictorSP(rDriveMotorPort2);
+		rDriveMotor3 = new VictorSP(rDriveMotorPort3);
+		
+		speedControllerShooter = new VictorSP(speedControllerPortShooter);
+		encoderShooter = new Encoder(encoderShooterPortA, encoderShooterPortB);
+		
+		//one side must be inverted for motors
+		leftDriveMotor = new SpeedControllerWrapper(lDriveMotor1, lDriveMotor2, lDriveMotor3);
+		rightDriveMotor = new SpeedControllerWrapper(rDriveMotor1, rDriveMotor2, rDriveMotor3);
+		leftDriveMotor.setInverted(true);
+		
+		driveTrain = new DriveTrain(); 
+		shooter = new Shooter();
+		boulderStager = new BoulderStager();
+	}
+	
+	
+	
+	
+
+
 	// For example to map the left and right motors, you could define the
 	// following variables to use with your drivetrain subsystem.
 	// public static int leftMotor = 1;
